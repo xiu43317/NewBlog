ed9a82ad7eeed35346c6cd8dfe8fefe07c19bdc9
Rock <xiu43317@gmail.com>
2020-11-16 12:00:21 +0800

    init

---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom'
+import App from './components/App';
+import { Provider } from 'react-redux'
+import AppReducer from './reducer/reducer';
+import { createStore } from 'redux';
+
+
+//import './index.css';
+ReactDOM.render(
+  (
+    <div>
+    </div>
+  ),
+  document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist/'),
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /.jsx$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(css)$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+
+  devServer: {
+    contentBase: './dist',
+    port: 8080,
+  },
+};
